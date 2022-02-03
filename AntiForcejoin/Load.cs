604a568189f95de48ebb52e47bbeff77f199c14a
Umbra <69671761+Umbra999@users.noreply.github.com>
2022-02-03 14:48:01 +0100

    Init

---
+++ AntiForcejoin/Load.cs
@@ -0,0 +1,80 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using VRC.Core;
+
+namespace AntiForcejoin
+{
+    public class Load : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            InitPatch();
+            Console.WriteLine("[AntiForcejoin] Loaded (https://github.com/Umbra999)");
+        }
+        private static readonly HarmonyLib.Harmony Instance = new HarmonyLib.Harmony("Graveyard");
+        private static HarmonyMethod GetPatch(string name)
+        {
+            return new HarmonyMethod(typeof(Load).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
+        }
+
+        private static void CreatePatch(MethodInfo TargetMethod, HarmonyMethod Before = null, HarmonyMethod After = null)
+        {
+            try
+            {
+                Instance.Patch(TargetMethod, Before, After);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to Patch {TargetMethod.Name} \n{e}");
+            }
+        }
+
+        public static unsafe void InitPatch()
+        {
+            CreatePatch(typeof(LoadBalancingPeer).GetMethod(nameof(LoadBalancingPeer.Method_Public_Virtual_New_Boolean_EnterRoomParams_2)), GetPatch(nameof(HandleInstanceFlow)));
+            CreatePatch(typeof(BestHTTP.HTTPManager).GetMethods().Where(m => m.Name == "SendRequest" && m.GetParameters().Length == 1).First(), GetPatch(nameof(HeaderPatch)));
+        }
+
+        // If you dirty skids gonna steal it give me atleast credits :*
+
+        // Telling the Photonserver to use a hidden instance type
+        private static void HandleInstanceFlow(ref EnterRoomParams __0)
+        {
+            Logger.Log(__0.field_Public_String_0);
+            if (__0.field_Public_String_0.Contains("~private") && __0.field_Public_String_0.Contains("~nonce") && __0.field_Public_String_0.Contains(APIUser.CurrentUser.id) && !__0.field_Public_String_0.Contains("~strict") && !__0.field_Public_String_0.Contains("~canRequestInvite"))
+            {
+                __0.field_Public_String_0 += "~strict";
+                Console.WriteLine("[AntiForcejoin] Forced instance to be non Forcejoinable");
+                MelonCoroutines.Start(WaitForInstance(__0.field_Public_String_0));
+            }
+        }
+
+        // To fix API Invites
+        private static IEnumerator WaitForInstance(string ID)
+        {
+            while (RoomManager.field_Internal_Static_ApiWorldInstance_0 == null) yield return null;
+            RoomManager.field_Internal_Static_ApiWorldInstance_0.instanceId = ID.Split(':')[1];
+        }
+
+        // To get a special Roomtoken for the Hidden instance
+        private static void HeaderPatch(BestHTTP.HTTPRequest request)
+        {
+            try
+            {
+                if (request.Uri.ToString().StartsWith("https://api.vrchat.cloud/api/1/instances/") && request.Uri.ToString().EndsWith("/join?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26&organization=vrchat") && request.Uri.ToString().Contains(APIUser.CurrentUser.id) && request.Uri.ToString().Contains("~private") && !request.Uri.ToString().Contains("~strict") && request.Uri.ToString().Contains("~nonce") && !request.Uri.ToString().Contains("~canRequestInvite"))
+                {
+                    string[] AdjustedURL = request.Uri.ToString().Split('/');
+                    string NewID = AdjustedURL[6] + "~strict";
+                    request.Uri = new Il2CppSystem.Uri(request.Uri.ToString().Replace(AdjustedURL[6], NewID));
+                    Console.WriteLine("[AntiForcejoin] Got Token for non forcejoinable Instance");
+                }
+            }
+            catch {  }
+        }
+    }
+}
---
+++ AntiForcejoin/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using AntiForcejoin;
+using MelonLoader;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("AntiForcejoin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntiForcejoin")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("13b85d59-ca85-44c8-8e3c-4d180de87538")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(Load), "Anti Forcejoin", "9.9.9", "Umbra", "https://disconnect.space")]
+[assembly: MelonGame("VRChat", "VRChat")]
